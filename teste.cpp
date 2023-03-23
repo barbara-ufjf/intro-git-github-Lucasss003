107defff0d6833279066c0482bd501c2106bbc17
Lucas_Henrique <lucashn003@hotmail.com>
2023-03-23 15:23:51 -0300

    selectshort

--- teste.cpp
+++ teste.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main()
+float* selectshort (float vet[], int n)
 {
-    cout << "Impressao teste na aula" << endl;
-    cout << "Impressao teste 2" << endl;
-    cout << "Impressao teste 3" << endl;
-    return 0;
+    int aux1;
+    for(int c=0;c<n;c++)
+        for(int i=c+1;i<n;i++)
+            if(vet[c]>vet[i])
+            {
+                aux1 = vet[c];
+                vet[c] = vet[i];
+                vet[i] = aux1;
+            }
+    return vet;
 }
 
+int main()
+{
+    float vet[] = {2,1,3,4,0};
+    int n = 5;
+    cout << "impressao antes da ordenacao:" << endl;
+    for(int c=0;c<n;c++)
+        cout << vet[c] << " ";
+    cout << endl;
+    selectshort(vet, n);
+    cout << "depois da ordenacao:" << endl;
+    for(int c=0;c<n;c++)
+        cout << vet[c] << " ";
+    cout << endl;
+    return 0;    
+}
