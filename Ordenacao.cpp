f07edcaf741b102c320f8550a226ecde2a597d83
Lucas_Henrique <lucashn003@hotmail.com>
2023-03-26 12:17:48 -0300

    3/4

--- Ordenacao.cpp
+++ Ordenacao.cpp
@@ -5,14 +5,14 @@ void BubbleSort(int vet[], int n)
 {
     int aux1;
     int comparacoes = 0, trocas = 0;
-    for (int c = 0; c < n; c++)
-        for (int i = c + 1; i < n; i++)
+    for (int c = n-1; c != 0; c--)
+        for (int i = 0; i < c; i++)
         {
             comparacoes++;
-            if (vet[c] > vet[i])
+            if (vet[i] > vet[i+1])
             {
-                aux1 = vet[c];
-                vet[c] = vet[i];
+                aux1 = vet[i+1];
+                vet[i+1] = vet[i];
                 vet[i] = aux1;
                 trocas++;
             }
@@ -30,7 +30,7 @@ void SelectSort(int vet[], int n)
 {
     int aux1, minimo,i;
     int comparacoes = 0, trocas = 0;
-    for (int c = 0; c < n; c++)
+    for (int c = 0; c < n-1; c++)
     {
         minimo = c;
         for (i = c+1 ; i < n; i++)
@@ -39,13 +39,10 @@ void SelectSort(int vet[], int n)
             if (vet[minimo] > vet[i])
                 minimo = i;
         }
-        if(minimo != c)
-        {
             aux1 = vet[minimo];
             vet[minimo] = vet[c];
             vet[c] = aux1;
             trocas++;
-        }
     }
     cout << "---------------------------------------------------" << endl;
     cout << "Metodo SelectSort: " << endl;
@@ -58,9 +55,21 @@ void SelectSort(int vet[], int n)
 
 void InsertShort(int vet[], int n)
 {
-    int aux1;
+    int pivo,i;
     int comparacoes = 0, trocas = 0;
-    
+    for(int c=1;c<n;c++)
+    {
+        pivo = vet[c];
+        i = c-1;
+        comparacoes++;
+        while(i>=0 && vet[i]>pivo)
+        {
+            vet[i+1] = vet[i];
+            i--;
+            trocas++;
+        }
+        vet[i+1] = pivo;
+    }
     cout << "---------------------------------------------------" << endl;
     cout << "Metodo InsertShort: " << endl;
     for (int c = 0; c < n; c++)
@@ -72,16 +81,16 @@ void InsertShort(int vet[], int n)
 
 int main()
 {
-    int vet[] = {2, 1, 3, 4, 0, -2, -5, 35};
+    int vet[] = {2, 1, 3, 4, 200, -2, -5, -88};
     int n = 8;
     cout << "Antes da ordenacao:" << endl;
     for (int c = 0; c < n; c++)
         cout << vet[c] << " ";
     cout << endl;
     BubbleSort(vet, n);
-    int vet2[] = {2, 1, 3, 4, 0, -2, -5, 35};
+    int vet2[] = {2, 1, 3, 4, 200, -2, -5, -88};
     SelectSort(vet2, n);
-    int vet3[] = {2, 1, 3, 4, 0, -2, -5, 35};
+    int vet3[] = {2, 1, 3, 4, 200, -2, -5, -88};
     InsertShort(vet3,n);
     cout << endl;
     return 0;
